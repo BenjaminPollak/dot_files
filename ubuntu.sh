c1c174cfca5d484ff0ffef22a87fe5da63548bc3
Benjamin <benjaminwpollak@gmail.com>
2024-01-16 23:31:44 -0500

    Added neovim install, stowing of neovim config

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,8 +3,4 @@ if [[ -z $STOW_FOLDERS ]]; then
   STOW_FOLDERS="nvim"
 fi
 
-if [[ -z $DOT_FILES ]]; then
-  DOTFILES=$HOME/dot_files
-fi
-
-STOW_FOLDERS=$STOW_FOLDERS DOTFILES=$DOTFILES $DOTFILES/install.sh
+STOW_FOLDERS=$STOW_FOLDERS DOTFILES=$DOTFILES ./install.sh
