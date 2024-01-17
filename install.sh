4c8442c3edf0fa42020d7c1d07ee57f2d0605862
Benjamin <benjaminwpollak@gmail.com>
2024-01-16 23:49:39 -0500

    Slightly modified scripts

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-pushd $DOTFILES
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
 do
   stow -D $folder
   stow $folder
 done
-popd
--- ubuntu.sh
+++ ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ -z $STOW_FOLDERS ]]; then
-  STOW_FOLDERS="nvim"
+  STOW_FOLDERS="nvim" # To add more, simply insert a comma and then a program name (no whitespace)
 fi
 
 STOW_FOLDERS=$STOW_FOLDERS DOTFILES=$DOTFILES ./install.sh
